d68320a29b77faf61515e716e799729c9de34e52
MatheusMoraes66 <matheus.csharp@hotmail.com>
2024-05-11 16:04:49 -0300

    test store in index

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import { useBoardStore } from '~/stores/boardStore';
+
+const boardStore = useBoardStore();
+
+</script>
+
 <template>
     <h1>Clone Trello</h1>
+    <pre>{{ boardStore.board }}</pre>
 </template>
