3ad35bfd587cfd19a26a49310f9b13ba17ee48d2
MatheusMoraes66 <matheus.csharp@hotmail.com>
2024-05-11 16:31:30 -0300

    create Ui and use components Next/ui

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,26 @@
 import { useBoardStore } from '~/stores/boardStore';
 
 const boardStore = useBoardStore();
-
 </script>
 
 <template>
-    <h1>Clone Trello</h1>
-    <pre>{{ boardStore.board }}</pre>
+    <div class="board-wrapper">
+        <main class="board">
+            <UContainer 
+                v-for="column in boardStore.board.columns" 
+                :key="column.name" 
+                class="column"
+            >
+                <h2 class="mb-4">{{ column.name }}</h2>
+                <ul>
+                    <li v-for="task in column.tasks" :key="task.id">
+                        <UCard class="mb-4">
+                            <strong>{{ task.name }}</strong>
+                            <p>{{ task.description }}</p>
+                        </UCard>
+                    </li>
+                </ul>
+            </UContainer>
+        </main>
+    </div>
 </template>
