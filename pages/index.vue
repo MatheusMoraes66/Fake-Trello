95d72972a209b7c2e141d4273dd0b80b0d2c9950
MatheusMoraes66 <matheus.csharp@hotmail.com>
2024-05-16 12:12:27 -0300

    Moving files to another folder

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,68 @@
 <script setup lang="ts">
-import { useBoardStore } from '~/stores/boardStore';
+import { useWorkSpaceStore } from '~/stores/workSpaceStore';
 
-const boardStore = useBoardStore();
-const route = useRoute();
-const router = useRouter();
+const isOpenWorkSpace = ref(false);
+const isBoards = ref(false);
+const selectWorkSpace = ref('');
 
-const newColumnName = ref('')
-const isModalOpen = computed(() => {
-    return route.name === 'index-tasks-id'
-})
+const workSpaceStore = useWorkSpaceStore();
 
-function addColumn() {
-    boardStore.addColumn(newColumnName.value)
-    newColumnName.value = ''
+if (workSpaceStore.store.length === 0) {
+    openModalWorkSpace();
 }
 
-function closeModal() {
-    router.push('/')
+function openModalWorkSpace() {
+    isOpenWorkSpace.value = true;
+}
+
+function closeModalWorkSpace() {
+    isOpenWorkSpace.value = false;
+}
+
+function openModalBoard(workspace: string) {
+    isBoards.value = true;
+    selectWorkSpace.value = workspace;
+}
+
+function closeModalBoard() {
+    isBoards.value = false;
 }
 
 </script>
 
 <template>
-    <div class="board-wrapper">
-        <main class="board">
-            <BoardColumn
-                v-for="(column, columnIndex) in boardStore.board.columns" 
-                :key="column.name"
-                :column="column"
-                :columnIndex="columnIndex"
-            />
-            <UContainer class="column">
-                <UInput 
-                    v-model="newColumnName"
-                    type="text" 
-                    placeholder="Add new Column" 
-                    icon="i-heroicons-plus-circle-solid" 
-                    @keyup.enter="addColumn"
-                />
-            </UContainer>
-        </main>
-        <div v-show="isModalOpen" class="task-bg" @click.self="closeModal">
-            <NuxtPage :key="route.fullPath"/>
+    <UContainer>
+        <div class="flex justify-between mt-4 mb-4">
+            <h1 class="text-2xl font-bold">Suas areas de trabalho</h1>
+            <UButton icon="i-heroicons-plus-16-solid" color="white" @click="openModalWorkSpace"></UButton>
         </div>
-    </div>
+        <UDivider  size="sm" />
+        <div v-if="workSpaceStore.store.length > 0" v-for="workSpace in workSpaceStore.store" class="mb-6 mt-3">
+            <div class="flex items-center w-auto h-12 mb-3 gap-2">
+                <UBadge color="gray" variant="solid" class="w-[30px] h-[30px] flex items-center justify-center">
+                    <UIcon :name="workSpace.type.icon" dynamic />
+                </UBadge>
+                <h2 class="font-bold text-lg">
+                    {{ workSpace.name }}
+                </h2>
+            </div>
+            <div class="grid lg:grid-cols-4 gap-4 sm:grid-cols-2 max-md:justify-center min-md:grid-cols-3 ">
+                <div v-for="board in workSpace.boards">
+                    <UCard :key="board.id" class="w-64 h-[8rem] flex items-center justify-center">
+                    <div class="flex justify-center items-center gap-3">
+                        <p class="text-xl">{{ board.name }}</p>
+                    </div>
+                </UCard>
+                </div>
+                <UCard class="w-64 h-[8rem] flex items-center justify-center" @click="openModalBoard(workSpace.id)">
+                    <div class="flex justify-center items-center gap-3">
+                        <UIcon name="i-heroicons-plus-circle-16-solid" dynamic />
+                        <p class="text-xl">Criar novo quadro</p>
+                    </div>
+                </UCard>
+            </div>
+        </div>
+    </UContainer>
+    <ModalWorkSpaceModal :is-open="isOpenWorkSpace" :close="closeModalWorkSpace" />
+    <ModalBoardModal :is-open="isBoards" :workspace="selectWorkSpace" :close="closeModalBoard"/>
 </template>
