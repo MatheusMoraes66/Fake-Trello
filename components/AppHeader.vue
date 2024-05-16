8d01769251db20b23522a6e6cca7558180d4447c
MatheusMoraes66 <matheus.csharp@hotmail.com>
2024-05-16 11:54:15 -0300

    fixed responsive layout

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -8,6 +8,7 @@ const keys = useMagicKeys();
 
 const CtrlK = keys['Ctrl+K'];
 const CmdK = keys['CMD+K'];
+
 watch(CtrlK, (v) => {
   if (v)
   emitOpenModal()
@@ -30,13 +31,13 @@ function emitOpenModal() {
 </script>
 
 <template>
-    <div class="w-full h-16 flex justify-around p-2 border-b-2 border-neutral-500">
+    <div class="w-full h-16 flex max-md:justify-between justify-around p-2 border-b-2 border-neutral-500">
         <div class=" flex items-center justify-start w-2/12 ">
             <img src="~/assets/images/logo.svg" alt="Logo" width="50" />
             <h2 class="font-mono font-bold">Trello</h2>
         </div>
-        <div class=" flex items-center justify-center w-4/12 ">
-            <UButton ui="{primary: 'black'}" @click="emitOpenModal" icon="i-heroicons-magnifying-glass-20-solid" block
+        <div class=" flex items-center justify-center w-4/12 max-md:hidden">
+            <UButton @click="emitOpenModal" icon="i-heroicons-magnifying-glass-20-solid" block
                  variant="solid" color="white">Pesquise... <UKbd>⌘ + K</UKbd>
             </UButton>
         </div>
