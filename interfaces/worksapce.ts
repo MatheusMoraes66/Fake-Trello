07825bc852f46aa98d329103ac5d471909d9aad1
MatheusMoraes66 <matheus.csharp@hotmail.com>
2024-05-18 11:01:24 -0300

    change interface attribute

--- interfaces/worksapce.ts
+++ interfaces/worksapce.ts
@@ -3,7 +3,7 @@ import type { Board } from "./board";
 export interface WorkSpace {
     id: string,
     name : string;
-    type : string;
+    type : { value : string, icon: string};
     description: string;
     boards: Board[];
 }
